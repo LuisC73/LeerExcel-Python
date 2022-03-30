a06343129e7baced6ac416be8ce62409c3a4b260
Luis Miguel Castro Curequia <80079884+LuisC73@users.noreply.github.com>
2022-03-30 11:07:27 -0500

    Se solicita el cliente a buscar

--- scripts/prueba.py
+++ scripts/prueba.py
@@ -1,51 +1,67 @@
 from fileinput import filename
 import pandas as pd
+try:
+    def main():
+        df = leer_archivo()
+        df = agregar_filtros(df)
 
-def main():
-    df = leer_archivo()
-    df = agregar_filtros(df)
+        exportar_datos(df)
+        # mostrar_datos(df)
 
-    exportar_datos(df)
-
-def leer_archivo():
-    print("Leyendo archivo.....")
-    import os
-    input_cols = [0,1,2]
-    path = r"C:\Users\Alejo\Documents\Luis\solo_python\prueba\input\."
-    filename =  input("Ingrese el nombre del archivo:  ") + ".xlsx"
-    fullpath = os.path.join(path,filename)
-    df = pd.read_excel(fullpath,
+    def leer_archivo():
+        print("Leyendo archivo.....")
+        import os
+        input_cols = [10,3,30,53,25,54,61]
+        filename =  input("Ingrese el nombre del archivo:  ") + ".xlsx"
+        if filename == "":
+            print("ERROR, Ingrese un archivo valido...")
+        path = r"D:\Users\practicante.geserv1\OneDrive - Centro de Servicios Mundial SAS\Imágenes\lm\Python\LeerExcel-Python\input\."
+        fullpath = os.path.join(path,filename)
+        df = pd.read_excel(fullpath,
                         sheet_name="Hoja1",
                         header= 0,
-                        usecols=input_cols)
-
-    return df
-
-
-def agregar_filtros(df):
-    print("Aplicando filtros.......")
-    df = df[df["NOMBRE"]=="MIGUEL"]
-
-    return df
+                        usecols=input_cols,
+                        converters={"CUPO_CREDITO":int})
+        
+        return df
 
 
-# print(df.shape)
-# df_cols = df.columns
+    def agregar_filtros(df):
+        print("Con que valor quiere realizar la busqueda : \n 1.ID \n 2.NOMBRE_CLIENTE")
+        buscar = int(input("Ingrese el valor: "))
+        if buscar == 1:
+            idCliente = input("Por favor ingrese el id del cliente.....")
+            print("Aplicando filtros.......")
+            df = df[df["NUMERO_DE_CUENTA"]==idCliente]
+        elif buscar == 2:
+            nombreCliente = input("Por favor ingrese el nombre del cliente....")
+            print("Aplicando filtros.......")
+            df = df[df["NOMBRE_CLIENTE"]==nombreCliente] 
+        else:
+            print("ERROR.....Ingrese un valor valido")
 
-# for col in df_cols:
-#     print(df[col].head(5))
+        return df
 
-def exportar_datos(df):
-    print("Exportando archivo procesado")
-    import os
-    path = r"C:\Users\Alejo\Documents\Luis\solo_python\prueba\output\."
-    name = input("Ingrese el nombre con el cual guardar el archivo: ") + ".xlsx"
-    fullname = os.path.join(path,name)
-    df.to_excel(fullname,
+    # def mostrar_datos(df):
+    #     print(df.shape)
+    #     df_cols = df.columns
+
+    #     for col in df_cols:
+    #         print(df[col].head(5))
+
+    def exportar_datos(df):
+        print("Exportando archivo procesado")
+        import os
+        path = r"D:\Users\practicante.geserv1\OneDrive - Centro de Servicios Mundial SAS\Imágenes\lm\Python\LeerExcel-Python\output\."
+        name = input("Ingrese el nombre con el cual guardar el archivo: ") + ".xlsx"
+        fullname = os.path.join(path,name)
+        df.to_excel(fullname,
             header= True,index = False)
 
 
-if __name__ == "__main__":
-    main()
-    input("\tPROCESO TERMINADO, presione enter para salir...")
+    if __name__ == "__main__":
+        main()
+        input("\tPROCESO TERMINADO, presione enter para salir...")
 
+except Exception as e:
+    print("ERROR EN EL SISTEMA: " + e)
